e757139a5492659817b1a0f228c93e7d8d7fb34b
gonzalo ruiz <38412697+gonzaruiz@users.noreply.github.com>
2018-08-07 23:09:45 -0500

    Add files via upload

---
+++ Assigment 16.1.py
@@ -0,0 +1,50 @@
+
+# coding: utf-8
+
+# In[88]:
+
+
+from statistics import variance
+
+
+# In[89]:
+
+
+list = [1550, 1700, 900, 850, 1000, 950]
+
+
+# In[90]:
+
+
+mean = statistics.mean (list)
+
+
+# In[91]:
+
+
+print (mean)
+
+
+# In[92]:
+
+
+median = statistics.median (list)
+
+
+# In[93]:
+
+
+print (median)
+
+
+# In[94]:
+
+
+stdeviation = statistics.stdev (list)
+
+
+# In[95]:
+
+
+print (stdeviation)
+
